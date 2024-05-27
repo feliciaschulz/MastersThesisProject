1d3691a3d586584e4e8cb766a89601d39e72f956
fschul <72020518+fschul@users.noreply.github.com>
2024-05-27 10:43:16 +0200

    Updating celltypes script

--- 03_DimensionalityReduction/check_celltypes.R
+++ 03_DimensionalityReduction/check_celltypes.R
@@ -1,38 +1,42 @@
 # Assuming you have your data frame "geo" with coordinates and a column "InferredCellType"
 # Replace these with your actual data
-geo <- read.csv("../00_Data/CellShapeData/R1B1ROI1_Cellshape.csv")
+geo <- read.csv("../00_Data/CellShapeData/R1D1ROI1_Cellshape.csv")
 # Load the ggplot2 package
 library(ggplot2)
 library(dplyr)
 
 # Define the color palette for the cell types
-color_palette <- c("Epithelial cells"= "red",     
-                   "Extracellular matrix"= "green",
-                   "Lymphatic vessel"= "blue",
-                   "Mixed"= "yellow",
-                   "Myocytes"= "maroon",
-                   "Other"= "cyan",
-                   "T-cells"= "magenta",
-                   "T-cells CD4+ (helper)"= "darkgreen",
+color_palette <- c("Epithelial cells"= "orange",     
+                   "Extracellular matrix"= "cyan",
+                   "Lymphatic vessel"= "yellow",
+                   "Mixed"= "darkgrey",
+                   "Myocytes"= "red",
+                   "Other"= "lightgrey",
+                   "T-cells Memory"= "magenta",
+                   "T-cells CD4+ (helper)"= "blue",
                    "T-cells Naive"= "navy",
-                   "T-cells Mixed"= "gray" )
+                   "T-cells Mixed"= "purple",
+                   "B-cells"= "darkgreen",
+                   "B-cells Plasma"= "lightgreen")
 
-grey_palette <- c("Epithelial cells"= "magenta",     
+grey_palette <- c("Epithelial cells"= "grey",     
                   "Extracellular matrix"= "grey",
                   "Lymphatic vessel"= "grey",
                   "Mixed"= "grey",
-                  "Myocytes"= "grey",
+                  "Myocytes"= "green",
                   "Other"= "grey",
-                  "T-cells"= "grey",
-                  "T-cells CD4+ (helper)"= "grey",
-                  "T-cells Naive"= "grey",
-                  "T-cells Mixed"= "grey" )
+                  "T-cells Memory"= "grey",
+                  "T-cells CD4+ (helper)"= "magenta",
+                  "T-cells Naive"= "purple",
+                  "T-cells Mixed"= "blue" )
 
+geo <- read.csv("../00_Data/CellShapeData/R1D1ROI1_Cellshape.csv")
 geo$InferredCellType = celltypes$InferredCellType
+
 # Create the scatter plot
 ggplot(geo, aes(x = Cell.Center.X, y = Cell.Center.Y, color = InferredCellType)) +
-  geom_point(size = 1) +  # Adjust point size as needed
-  scale_color_manual(values = grey_palette) +  # Assign colors based on the palette
+  geom_point(size = 1.5) +  # Adjust point size as needed
+  scale_color_manual(values = color_palette) +  # Assign colors based on the palette
   labs(x = "X", y = "Y", title = "Colored Scatter Plot with Strings") +
   theme_minimal() +
   theme(legend.position = "right")  # Adjust legend position as needed
@@ -45,7 +49,7 @@ geo$ID = rownames(geo)
 merged_geo <- inner_join(geo, averages_tomerge, by = "ID")
 
 # Create the scatter plot with color based on intensity values
-ggplot(merged_geo, aes(x = Cell.Center.X, y = Cell.Center.Y, color = cytokeratin)) +  # Replace IntensityColumn with the actual column name
+ggplot(merged_geo, aes(x = Cell.Center.X, y = Cell.Center.Y, color = cd45ro)) +  # Replace IntensityColumn with the actual column name
   geom_point(size = 1) +  # Adjust point size as needed
   scale_color_gradient(low = "blue", high = "red") +  # Adjust the gradient colors as needed
   labs(x = "X", y = "Y", title = "Colored Scatter Plot Based on Intensity Values") +
@@ -54,7 +58,7 @@ ggplot(merged_geo, aes(x = Cell.Center.X, y = Cell.Center.Y, color = cytokeratin
  
 
 
-
+colnames(averages)
 
 
 
